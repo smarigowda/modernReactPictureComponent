c5b7ed394150c7cf1feda756eba027af4d4461cb
Santosh Marigowda <santosharakere@gmail.com>
2019-04-12 06:57:03 +0100

    handling onChange and onClick events for input element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { searchInput } from './SearchBar.module.scss';
 
 const SearchBar = () => {
+    const onChangeInput = event => {
+        console.log(event.target.value);
+    }
+    const onClickInput = event => {
+        console.log('input clicked...');
+    }
     return (
         <form action="#">
-            <input className={searchInput} type="text"/>
+            <label htmlFor="">Image Search</label>
+            <input
+                className={searchInput}
+                type="text"
+                onChange={onChangeInput}
+                onClick={onClickInput}/>
         </form>
     )
 };
