3fda7c9a9fc12bff1def6a43cd9c148f150dfcad
Santosh Marigowda <santosharakere@gmail.com>
2019-04-16 21:30:44 +0100

    dynamically setting grid row end

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,24 +1,32 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { image } from './ImageCard.module.scss';
 const ImageCard = ({ item }) => {
     let imageRef = useRef(null);
+    let [spans, setSpans] = useState(0);
+    const handler = () => {
+        console.log('image loaded');
+        console.dir(imageRef.current.clientHeight);
+        setSpans(Math.ceil(imageRef.current.clientHeight / 10));
+    }
     useEffect(() => {
+        console.log('-- running useEffect --');
+        console.log(`-- spans = ${spans}`);
         console.dir(imageRef.current);
-        imageRef.current.addEventListener('click', () => {
-            console.log('image clicked');
-        })
+        imageRef.current.addEventListener('load', handler)
         return () => {
             console.log('remove event listener here');
-            imageRef.current.removeEventListener('click');
+            imageRef.current.removeEventListener('click', handler);
         }
     });
 
     return (
-        <img
-            className={image}
-            ref={imageRef}
-            src={item.url}>
-        </img>
+        <div style={ { gridRowEnd: `span ${spans}`}}>
+            <img
+                className={image}
+                ref={imageRef}
+                src={item.url}>
+            </img>
+        </div>
     );
 }
 
