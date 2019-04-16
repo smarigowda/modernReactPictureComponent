52f09bbb692d7bd6a5bdb6adcdf9f7550760e30e
Santosh Marigowda <santosharakere@gmail.com>
2019-04-16 21:41:58 +0100

    commented console log

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,17 +4,17 @@ const ImageCard = ({ item }) => {
     let imageRef = useRef(null);
     let [spans, setSpans] = useState(0);
     const handler = () => {
-        console.log('image loaded');
-        console.dir(imageRef.current.clientHeight);
+        // console.log('image loaded');
+        // console.dir(imageRef.current.clientHeight);
         setSpans(Math.ceil(imageRef.current.clientHeight / 10));
     }
     useEffect(() => {
-        console.log('-- running useEffect --');
-        console.log(`-- spans = ${spans}`);
-        console.dir(imageRef.current);
+        // console.log('-- running useEffect --');
+        // console.log(`-- spans = ${spans}`);
+        // console.dir(imageRef.current);
         imageRef.current.addEventListener('load', handler)
         return () => {
-            console.log('remove event listener here');
+            // console.log('remove event listener here');
             imageRef.current.removeEventListener('click', handler);
         }
     });
